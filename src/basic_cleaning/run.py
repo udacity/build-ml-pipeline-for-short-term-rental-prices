48663471003158d36703805173f28996d24335be
Bela Pires <belapires@MacBook-Air-de-Bela-2.local>
2022-08-04 20:03:14 +0100

    fix data cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -32,6 +32,10 @@ def go(args):
     logger.info("Converting last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    logger.info("Dropping improper locations")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logging.info("Saving clean file")
     df.to_csv(args.output_artifact, index=False)
 
