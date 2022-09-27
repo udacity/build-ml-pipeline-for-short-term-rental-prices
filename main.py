b777ba9ac20b5c87378d88c3c1a9da61217def15
Sejong Heo <tromberx@gmail.com>
2022-09-28 08:22:37 +0900

    update: step 3-data_check

--- main.py
+++ main.py
@@ -58,12 +58,12 @@ def go(config: DictConfig):
                              "src", "basic_cleaning"),
                 "main",
                 parameters={
-                        "input_artifact": "sample.csv:latest",
-                        "output_artifact": "clean_sample.csv",
-                        "output_type": "clean_sample",
-                        "output_description": "Data with outliers and null values removed",
-                        "min_price": config['etl']['min_price'],
-                        "max_price": config['etl']['max_price']
+                    "input_artifact": "sample.csv:latest",
+                    "output_artifact": "clean_sample.csv",
+                    "output_type": "clean_sample",
+                    "output_description": "Data with outliers and null values removed",
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
                 },
             )
 
@@ -71,7 +71,18 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(),
+                             "src", "data_check"),
+                "main",
+                parameters={
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
+                    "kl_threshold": config['data_check']['kl_threshold'],
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
+                },
+            )
 
         if "data_split" in active_steps:
             ##################
