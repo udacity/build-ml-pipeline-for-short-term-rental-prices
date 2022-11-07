f7a273c85f23cab224f2b8b9c0d59fb7d6daa40e
Alexander Bondarenko <alexander.bondarenko@shippeo.com>
2022-11-07 17:51:26 +0100

    debugging

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def go(config: DictConfig):
         if "download" in active_steps:
             # Download file and load in W&B
             _ = mlflow.run(
-                os.path.join(config['main']['components_repository'], "components"),
+                config['main']['components_repository'],
                 "main",
                 parameters={
                     "sample": config["etl"]["sample"],
