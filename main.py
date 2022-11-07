8a79b8d1358144f83f8cdf106013aae9d5a40348
Alexander Bondarenko <alexander.bondarenko@shippeo.com>
2022-11-07 16:19:28 +0100

    changed to pip in conda.yml

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ from omegaconf import DictConfig
 
 _steps = [
     "download",
-    "basic_cleaning",
-    "data_check",
-    "data_split",
-    "train_random_forest",
+    # "basic_cleaning",
+    # "data_check",
+    # "data_split",
+    # "train_random_forest",
     # NOTE: We do not include this in the steps so it is not run by mistake.
     # You first need to promote a model export to "prod" before you can run this,
     # then you need to run this step explicitly
