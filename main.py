5d39b9639fb64107be975569e3b574ab0fae2ee7
Alexander Bondarenko <alexander.bondarenko@shippeo.com>
2022-11-08 16:09:24 +0100

    test data completed

--- main.py
+++ main.py
@@ -69,7 +69,18 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src",
+                             "data_check"),
+                "main",
+                parameters={
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
+                    "kl_threshold": "1.0",
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
+                },
+            )
 
         if "data_split" in active_steps:
             ##################
