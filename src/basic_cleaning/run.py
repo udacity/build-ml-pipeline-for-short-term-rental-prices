0e1f7cccb342922ce96066a66700d2384af8e0d3
Nikolas Hunt <nikolas.hunt@dyson.com>
2022-11-21 10:53:04 +0000

    Add basic_cleaning component using cookiecutter

---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+"""
+Performs basic cleaning of W&B dataset and saves results to W&B.
+"""
+import argparse
+import logging
+import wandb
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(job_type="basic_cleaning")
+    run.config.update(args)
+
+    # Download input artifact. This will also log that this script is using this
+    # particular version of the artifact
+    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+
+    ######################
+    # YOUR CODE HERE     #
+    ######################
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Performs basic cleaning of data")
+
+
+    parser.add_argument(
+        "--parameter1", 
+        type=## INSERT TYPE HERE: str, float or int,
+        help=## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--parameter2", 
+        type=## INSERT TYPE HERE: str, float or int,
+        help=## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+
+    args = parser.parse_args()
+
+    go(args)
