24e0aeaf3dd860e07781696c8c2278087ff3d626
Arun Prabhath <arunprabhath2010@gmail.com>
2023-01-01 18:40:51 +0530

    completed basic_cleaning_step

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python
 """
-Performs basic cleaning on the data and save the results in Weights & Biases
+Download from W&B the raw dataset and apply some basic data cleaning,
+exporting the result to a new artifact
 """
 import argparse
 import logging
+import os
 import wandb
+import pandas as pd
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
@@ -12,47 +14,97 @@ logger = logging.getLogger()
 
 
 def go(args):
-
+    """
+    Basic cleaning procedure
+    """
     run = wandb.init(job_type="basic_cleaning")
     run.config.update(args)
-    print("Test run completed")
 
-    # Download input artifact. This will also log that this script is using this
-    # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    dataframe = pd.read_csv(artifact_local_path, index_col="id")
+    min_price = args.min_price
+    max_price = args.max_price
+    idx = dataframe['price'].between(min_price, max_price)
+    dataframe = dataframe[idx].copy()
+    logger.info("Dataset price outliers removal outside range: %s-%s",
+                 args.min_price, args.max_price)
+    dataframe['last_review'] = pd.to_datetime(dataframe['last_review'])
+    logger.info("Dataset last_review data type fix")
+
+    idx = dataframe['longitude'].between(-74.25, -73.50) & dataframe['latitude'].between(40.5, 41.2)
+    dataframe = dataframe[idx].copy()
+
+    tmp_artifact_path = os.path.join(args.tmp_directory, args.output_artifact)
+    dataframe.to_csv(tmp_artifact_path)
+    logger.info("Temporary artifact saved to %s" , tmp_artifact_path)
+
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description
+    )
+
+    artifact.add_file(tmp_artifact_path)
+    run.log_artifact(artifact)
+
+    artifact.wait()
+    logger.info("Cleaned dataset uploaded to wandb")
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="This steps cleans the data")
-
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
 
     parser.add_argument(
-        "--parameter1", 
+        "--tmp_directory",
         type=str,
-        help="",
+        help="Temporary directory for dataset storage",
         required=True
     )
 
     parser.add_argument(
-        "--parameter2", 
+        "--input_artifact",
         type=str,
-        help="",
+        help="Input artifact name",
         required=True
     )
 
     parser.add_argument(
-        "--parameter3", 
+        "--output_artifact",
         type=str,
-        help="",
+        help="Output artifact name",
         required=True
     )
 
+    parser.add_argument(
+        "--output_type",
+        type=str,
+        help="Output artifact type",
+        required=True
+    )
 
-    args = parser.parse_args()
+    parser.add_argument(
+        "--output_description",
+        type=str,
+        help="Output artifact description",
+        required=True
+    )
 
-    go(args)
+    parser.add_argument(
+        "--min_price",
+        type=int,
+        help="Minimum price limit",
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price",
+        type=int,
+        help="Maximum price limit",
+        required=True
+    )
+
+    main_args = parser.parse_args()
+
+    go(main_args)
