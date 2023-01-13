afdfdd9ec33ce7f5e4ca9cffd40403ae1b0c3035
Edmund Home <edmund.home@sequra.es>
2023-01-14 00:11:51 +0100

    added data_split step

--- main.py
+++ main.py
@@ -65,6 +65,7 @@ def go(config: DictConfig):
             )
 
         if "data_check" in active_steps:
+            # Check cleaned data is correct
             _ = mlflow.run(
                 os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
                 "main",
@@ -78,10 +79,16 @@ def go(config: DictConfig):
             )
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            # Split data into train, validation and test
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                version='main',
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                },
+            )
 
         if "train_random_forest" in active_steps:
 
