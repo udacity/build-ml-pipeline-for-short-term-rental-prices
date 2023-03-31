a4a0514836774e9f4fa3e88da44e95217f4af8b0
Adam Arnon <adamarnon@gmail.com>
2023-03-31 11:51:02 -0700

    Working pipeline

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,16 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+def test_row_count(data: pd.DataFrame):
+    """
+    Test that the number of rows in the dataset is between 15000 and 1000000
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    Test that the minimum price is between the given limits
+    """
+    assert data['price'].between(min_price, max_price).all()
+
+
