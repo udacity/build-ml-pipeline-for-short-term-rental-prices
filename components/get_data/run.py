036c098e66deac11770445da317f23093f542c0f
Adam Arnon <adamarnon@gmail.com>
2023-03-31 17:08:20 -0700

    Fixed split component

--- components/get_data/run.py
+++ components/get_data/run.py
@@ -33,14 +33,14 @@ def go(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download URL to a local destination")
 
-    parser.add_argument("sample", type=str, help="Name of the sample to download")
+    parser.add_argument("--sample", type=str, help="Name of the sample to download")
 
-    parser.add_argument("artifact_name", type=str, help="Name for the output artifact")
+    parser.add_argument("--artifact_name", type=str, help="Name for the output artifact")
 
-    parser.add_argument("artifact_type", type=str, help="Output artifact type.")
+    parser.add_argument("--artifact_type", type=str, help="Output artifact type.")
 
     parser.add_argument(
-        "artifact_description", type=str, help="A brief description of this artifact"
+        "--artifact_description", type=str, help="A brief description of this artifact"
     )
 
     args = parser.parse_args()
