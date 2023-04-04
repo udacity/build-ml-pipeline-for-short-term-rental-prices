ecf46fdb90600190ebea78d7a81c53eccb4cf87f
Jonathan Ratschat <jonathan.ratschat@ing.com>
2023-04-04 16:08:34 +0200

    Add data_check functionality

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -42,17 +42,23 @@ def test_neighborhood_names(data):
 
 def test_proper_boundaries(data: pd.DataFrame):
     """
-    Test proper longitude and latitude boundaries for properties in and around NYC
+    Test proper longitude and latitude boundaries for properties in and around NYC.
     """
-    idx = data['longitude'].between(-74.25, -73.50) & data['latitude'].between(40.5, 41.2)
+    idx = (
+        data['longitude'].between(-74.25, -73.50) & data['latitude'].between(40.5, 41.2)
+    )
 
     assert np.sum(~idx) == 0
 
 
-def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_threshold: float):
+def test_similar_neigh_distrib(
+        data: pd.DataFrame,
+        ref_data: pd.DataFrame,
+        kl_threshold: float
+        ):
     """
-    Apply a threshold on the KL divergence to detect if the distribution of the new data is
-    significantly different than that of the reference dataset
+    Apply a threshold on the KL divergence to detect if the distribution of the new data
+    is significantly different than that of the reference dataset.
     """
     dist1 = data['neighbourhood_group'].value_counts().sort_index()
     dist2 = ref_data['neighbourhood_group'].value_counts().sort_index()
@@ -60,6 +66,16 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data):
+    """
+    Test if data set is reasonable small or large by checking if the number of rows is
+    between 15000 and 1000000.
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data, min_price, max_price):
+    """
+    Test if all prices are between min_price and max_price.
+    """
+    assert data['price'].between(min_price, max_price).all()
