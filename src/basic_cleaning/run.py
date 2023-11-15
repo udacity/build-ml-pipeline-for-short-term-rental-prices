5e113ef70a2b080a704ad22a1b507c8b2a9b0ceb
Geawher <jawherjabri2000@gmail.com>
2023-11-15 16:18:08 +0100

    fixing longitude & latitude

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -28,6 +28,9 @@ def go(args):
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy() 
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
