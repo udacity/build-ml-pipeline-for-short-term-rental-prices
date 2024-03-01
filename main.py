6f9b87d1d2b4b0a37cc2e947c51411c45d75600f
Sabari Manohar <manoharsabari1@gmail.com>
2024-03-01 07:57:34 +0000

    complete basic setup

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def go(config: DictConfig):
         if "test_regression_model" in active_steps:
 
             _ = mlflow.run(
-                f"{config['main']['components_repository']}/test_regression_model",
+                os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
                 "main",
                 parameters={
                     "mlflow_model": "random_forest_export:prod",
